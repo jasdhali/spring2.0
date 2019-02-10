08216698ed879875ee55738bdf0acc4bf592827e
manisha <jasdhali@gmail.com>
2019-02-10 11:02:33 -0600

    New spring boot for demo Hikari and MySql

---
+++ springboot-hikari-mysql/springboot-hikari-mysql/src/main/java/com/example/springboothikarimysql/SpringbootHikariMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboothikarimysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHikariMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHikariMysqlApplication.class, args);
+	}
+
+}
+
